e6b72296fab2a29fcc406d55eced3e4d332abbcc
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-08-12 13:25:23 +0700

    update

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,8 +2,9 @@ var userLogin = document.getElementById("user__login")
 var userRegister = document.getElementById("user__register")
 var openModalLogin = document.getElementById("open-login")
 var openModalRegister = document.getElementById("open-register")
-
-
+var closeNavbar = document.getElementsByClassName("close-navbar")
+var navBar = document.getElementById("header-navbar")
+var controlNavbar = document.getElementById("control-navbar")
 
 closeModal = function(){
     modal.style.display = "none"
@@ -23,4 +24,15 @@ openRegister = function(){
     openModal();
     userLogin.style.display = "none"
     userRegister.style.display = "block"
+}
+
+showNavbar = function(){
+    if (navBar.style.display === 'none'){
+        console.log("Ok")
+    }
+    navBar.style.display = "flex";
+}
+controlNavbar.onclick = function(){
+    console.log(navBar.style.display === 'none')
+    showNavbar()
 }
