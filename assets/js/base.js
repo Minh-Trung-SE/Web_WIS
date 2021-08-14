9a55f6e63ce1fa10dd2e6b301aa6e0ecd2673449
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-08-15 00:13:45 +0700

    fix bug

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,38 +1,75 @@
-var userLogin = document.getElementById("user__login")
-var userRegister = document.getElementById("user__register")
-var openModalLogin = document.getElementById("open-login")
-var openModalRegister = document.getElementById("open-register")
-var closeNavbar = document.getElementsByClassName("close-navbar")
-var navBar = document.getElementById("header-navbar")
-var controlNavbar = document.getElementById("control-navbar")
+let navbarControler = document.getElementById('header')
+let userLoginBtn = document.getElementById('user-login')
+let userRegisterBtn = document.getElementById('user-register')
+let modalWrapper = document.getElementsByClassName('modal-wrapper')[0]
+// console.log(modalWrapper)
+// console.log(navbarControler.classList)
+// console.log(userRegisterBtn)
 
-closeModal = function(){
-    modal.style.display = "none"
+function openLogin(){
+    openModalWrapper()
+    userLoginBtn.classList.add('modal--active')
 }
 
-openModal= function(){
-    modal.style.display = "flex"
+function openRegister(){
+    openModalWrapper()
+    userRegisterBtn.classList.add('modal--active')
 }
 
-openLogin = function(){
-    openModal();
-    userLogin.style.display = "block"
-    userRegister.style.display = "none"
-}
-
-openRegister = function(){
-    openModal();
-    userLogin.style.display = "none"
-    userRegister.style.display = "block"
-}
-
-showNavbar = function(){
-    if (navBar.style.display === 'none'){
-        console.log("Ok")
+function closeUserForm(){
+    if(userLoginBtn.classList.contains('modal--active')){
+        userLoginBtn.classList.remove('modal--active')
     }
-    navBar.style.display = "flex";
+
+    if(userRegisterBtn.classList.contains('modal--active')){
+        userRegisterBtn.classList.remove('modal--active')
+    }
+    closeModalWrapper()
 }
-controlNavbar.onclick = function(){
-    console.log(navBar.style.display === 'none')
-    showNavbar()
+
+function openModalWrapper(){
+    modalWrapper.classList.add('modal-wrapper--active')
 }
+
+function closeModalWrapper(){
+    if(modalWrapper.classList.contains('modal-wrapper--active')){
+        modalWrapper.classList.remove('modal-wrapper--active')
+    }
+}
+function openMobileMenu(){
+    navbarControler.classList.add("mobile--active")
+}
+
+function closeMobileMenu(){
+    if(navbarControler.classList.contains("mobile--active")){
+        navbarControler.classList.remove("mobile--active")
+    }
+}
+
+// var userLogin = document.getElementById("user__login")
+// var userRegister = document.getElementById("user__register")
+// var openModalLogin = document.getElementById("open-login")
+// var openModalRegister = document.getElementById("open-register")
+// var closeNavbar = document.getElementsByClassName("close-navbar")
+// var navBar = document.getElementById("header-navbar")
+// var controlNavbar = document.getElementById("control-navbar")
+
+// closeModal = function(){
+//     modal.style.display = "none"
+// }
+
+// openModal= function(){
+//     modal.style.display = "flex"
+// }
+
+// openLogin = function(){
+//     openModal();
+//     userLogin.style.display = "block"
+//     userRegister.style.display = "none"
+// }
+
+// openRegister = function(){
+//     openModal();
+//     userLogin.style.display = "none"
+//     userRegister.style.display = "block"
+// }
